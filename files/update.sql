f78854453542ce4abb14db1cc69af4630776428b
younex1 <info@younex.de>
2024-02-20 23:16:40 +0100

    Update update.sql

--- files/update.sql
+++ files/update.sql
@@ -1,1 +1,1 @@
-UPDATE ad_column SET isalwaysupdateable = 'Y',isupdateable = 'Y' WHERE ad_column_id = 2175;
+"UPDATE ad_column SET isalwaysupdateable = 'Y',isupdateable = 'Y' WHERE ad_column_id = 2175;"
