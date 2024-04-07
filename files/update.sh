2f2bcf9d8c7e4aeb5a84bc9d9744d20e69b5f71d
younex1 <info@younex.de>
2024-04-07 23:32:51 +0200

    Update update.sh

--- files/update.sh
+++ files/update.sh
@@ -2,6 +2,7 @@
 export PGPASSWORD='MyWeak_Pa55w0rd'
 #psql -h '127.0.0.1' -U 'adempiere' -d 'idempiere' -c "UPDATE ad_column SET isalwaysupdateable = 'Y',isupdateable = 'Y' WHERE ad_column_id = 2175"
 psql -h '127.0.0.1' -U 'adempiere' -d 'idempiere' -f /opt/sql/migration/files/update.sql
+psql -h '127.0.0.1' -U 'adempiere' -d 'idempiere' -f /opt/sql/migration/files/german.sql
 
 curl --insecure --location --request DELETE 'http://127.0.0.1:8080/api/v1/caches' \
 --header 'Content-Type: application/json' \
