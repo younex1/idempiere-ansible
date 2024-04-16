1001d0e36f06279d6f6f5cd139a677f09bbcf398
younex1 <info@younex.de>
2024-04-16 12:08:20 +0200

    Update create-strapi-db.sh

--- files/create-strapi-db.sh
+++ files/create-strapi-db.sh
@@ -14,4 +14,5 @@ sudo -u $DB_USER psql -c "CREATE DATABASE $NEW_DB_NAME;"
 
 # Create the user and grant privileges
 sudo -u $DB_USER psql -c "CREATE USER $NEW_DB_USER WITH PASSWORD '$NEW_DB_PASS';"
-sudo -u $DB_USER psql -c "GRANT ALL PRIVILEGES ON DATABASE $NEW_DB_NAME TO $NEW_DB_USER;"
+sudo -u $DB_USER psql -d $NEW_DB_NAME -c "GRANT ALL PRIVILEGES ON DATABASE $NEW_DB_NAME TO $NEW_DB_USER;"
+sudo -u $DB_USER psql -d $NEW_DB_NAME -c "GRANT USAGE ON SCHEMA public TO $NEW_DB_NAME;"
